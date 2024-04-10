ceb65eb186a4765a1cb45f55599164ec634ce35a
BirkofNiki <augastinendeti@gmail.com>
2024-04-10 22:40:12 +0300

    added the schema.ts that tells what goes to the database

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,17 @@
 //defines what kind of data structures live in the database
 //doing it below:
-import {pgTable, text} from "drizzle-orm/pg-core";
+
+import { doublePrecision, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const productsTable=pgTable("products",{
-    id: text ("id").products
+    id: text ("id").primaryKey().default("uuid_generate_v4()"),
+    name: text("name").notNull(),
+    imageId: text("imageId").notNull(),
+    price: doublePrecision("price").notNull(),
+    description: text("description"),
+    createdAt: timestamp("createdAt").defaultNow(),
+    updatedAt: timestamp("updatedAt").defaultNow(),
+
+
 })
+export type Product = typeof productsTable.$inferSelect

55f7e736feb0177482d9fce7543442c2aea9baac
BirkofNiki <augastinendeti@gmail.com>
2024-04-10 15:40:12 +0300

    added the search functionality on the SearchBar component

--- README.md
+++ README.md
@@ -34,3 +34,6 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+JUST OTHER STUFF:
+How to install search button..use shadcn ; i.e npx shadcn-ui@latest add button
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,51 @@
+"use client" //since this is a server component, without this use client it won't work on clinet side.
+import { Search } from "lucide-react"
+import { Button } from "./ui/button"
 import {Input} from "./ui/input"
+import { useRef, useState, useTransition } from "react"
+import { useRouter } from "next/navigation"
+import { staticGenerationAsyncStorage } from "next/dist/client/components/static-generation-async-storage.external"
 const SearchBar = () => {
+const inputRef= useRef<HTMLInputElement>(null)
+
+//creating the search functionality starts below, tells what exactly happens on clicking enter
+const [isSearching, startTransition] = useTransition() //hooks into the page switching cycle
+const router= useRouter ()
+const [query,setQuery]=useState<string>()
+
+const search = () => {
+    startTransition(() => {
+        router.push("/search?=${query}")
+
+    })
+
+} //this an arrow function
+
     return (
         <div className="relative w-full h-14 flex flex-col bg-white">
             <div className="relative h-14 z-10 rounded-md">
-                <Input className="absolute inset-0 h-full" />
+                <Input
+                disabled={isSearching}
+                value={query} 
+                onChange={(e)=>setQuery(e.target.value)}
+                onKeyDown={(e) =>{
+                    if(e.key==="Enter") {
+                        search()
+                    }
 
+                    if(e.key === "Escape") {
+                        inputRef?.current?.blur()
+                    }
+
+                }}
+                 ref={inputRef} className="absolute inset-0 h-full" />
+
+<Button 
+    size="lg"
+    onClick={search}
+    className="absolute right-0 inset-y-0 h-full rounded-1-none">
+        <Search className="h-6 w-6"/>
+</Button>
             </div>
 
         </div>
